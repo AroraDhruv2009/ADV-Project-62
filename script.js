31ec70170b5bbcbbfc03be96584874c8abd766d6
Dhruv Arora <78738856+AroraDhruv2009@users.noreply.github.com>
2021-02-18 19:16:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function Click1() {
+    window.alert(" Former thief Scott Lang once stole an advanced size-altering suit in order to aid his ailing daughter, only to discover that the stolen tech belonged to the world-renowned Dr. Hank Pym. Seeing the heroic potential within him, Dr. Pym allowed Scott to continue using the suit, as well as the identity Pym once battled evil under. As the Astonishing Ant-Man, Scott now handles the jobs for any other Super Hero.");
+}
+
+function Click2() {
+    window.alert(" About Black Panther - Monarch of the secluded but technologically advanced African nation of Wakanda, King TChalla is the Black Panther; a sacred title that must be both inherited & earned by the current Wakandan ruler. Granted superhuman powers by ceremonially consuming a mystical heart-shaped herb, the Black Panther is responsible for defending his people, and the world, from any threats.");
+}
+
+function Click3() {
+    window.alert(" About Captain America - During WWII, the patriotic Steve Rogers was offered a place in the military's top operation: Rebirth. Injected with an experimental super-serum, Rogers emerged from the treatment with heightened endurance, strength, and reaction time. With extensive training and an indestructible Vibranium shield, Rogers soon became the country's ultimate weapon: CAPTAIN AMERICA! Though frozen in ice during a climactic battle toward the end of the war, Rogers was discovered and revived decades later. Now the living legend continues the war against evil in modern times as a member of The Avengers!")
+}
+
+function Click4() {
+    window.alert(" About Doctor Strange - Distinguished neurosurgeon Doctor Stephen Strange's self-important worldview was shattered, along with his steady operating hands, in a fateful car accident. Desperate to heal his injuries and pride, Strange sought out a legendary safe known as the Ancient One, becoming a pupil of the mystic arts. Learning that his true worth had always come from within, Strange uncovered his full potential and was chosen to become the Sorcerer Supreme, the world's pre-eminent defender against the darkness that lurks beyond.")
+}
+
+function Click5() {
+    window.alert("About Hulk - A massive dose of gamma radiation transformed the brilliant but meek scientist Bruce Banner's DNA, awakening the hidden, adrenaline-fed hero in his genes... HULK! A hero of few words and incredible strength, the Hulk has long been pursued by those who want to use his immense power for their own purposes, or by those who thought the Jade Giant's anger was too dangerous to be controlled. Now, as a member of the Avengers, Hulk helps smash the unimaginable threats that no Hero could face alone, hoping to at least prove to the world that he is the strongest HERO there is!")
+}
+
+function Click6() {
+    window.alert("About Nick Fury - Nick Fury became an elite member of the U.S. intelligence team because of intellect and integrity. He has been trained as a paratrooper, Ranger, demolitions expert and vehicle specialist. Nick Fury keeps his youth by the mysterious Infinity Formula. When the world is in trouble, NICK FURY: AGENT OF S.H.I.E.L.D. is the man who will defend it.")
+}
+
+function Click7() {
+    window.alert("About Iron Man - When billionaire industrialist Tony Stark dons his sophisticated steel-mesh armor, he becomes a living high-tech weapon - the world's greatest fighting machine. Tony has primed his ultra modern creation for waging state of the art campaigns, attaining sonic flight, and defending the greater good! He is the Armored Avenger - driven by a heart that is part machine, but all hero! He is the INVINCIBLE IRON MAN!")
+}
+
+function Click8() {
+    window.alert("About Thor - Nordic legend tells the tale of the son of Odin, the heir to the throne of Asgard - he is THOR, renowned as the mightiest hero of mythology! Thor's strength, endurance, and quest for battle are far greater than his Asgardian brethren. The mighty Thor wields an enchanted Uru hammer, Mjolnir, and is master of thunder and lightning.")
+}
